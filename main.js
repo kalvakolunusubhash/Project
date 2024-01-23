39fdcbcc0066a0c467b6ddbb899eb28cb6c14c07
kalvakolunusubhash <156099250+kalvakolunusubhash@users.noreply.github.com>
2024-01-23 21:40:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+function hide(){
+  if (document.getElementById("hideme").style.display === "none") {
+    document.getElementById("hideme").style.display="block";
+  } else {
+    document.getElementById("hideme").style.display="none";
+  }
+}
